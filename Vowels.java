4efcc79f8284150d11239d584d165e0bce29c3c9
jeevajeeva <vjeeva522@gmail.com>
2017-07-03 20:27:41 +0530

    Create Vowels.java

---
+++ Vowels.java
@@ -0,0 +1,20 @@
+# project3
+import java.io.*;
+import java.util.*;
+public class Vowels
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    char s;
+    s=sc.next().charAt(0);
+    if(((s=='a')||(s=='e')||(s=='i')||(s=='o')||(s=='u')) ||(s=='A')||(s=='E')||(s=='I')||(s=='O')||(s=='U'))
+    {
+      System.out.println("vowels");
+    }
+    else
+    {
+      System.out.println("constant");
+    }
+  }
+}
